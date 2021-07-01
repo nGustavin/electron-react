dbb3eb4673d2ecccca9a8258b75cdc2921fbfd85
ngustavin <gust4f3rn@gmail.com>
2021-07-01 02:20:39 -0300

    Configuring linting

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,18 @@
+// import { ReactNode } from 'react';
+import React from 'react'
+
+import { Container } from './styles';
+
+// interface HomeProps {
+//   children?: ReactNode;
+// }
+
+function Home() {
+  return (
+    <Container>
+      
+    </Container>
+  );
+};
+
+export default Home;
